8272253bc3b7e47fcae1829dc9fc1afedecceed7
Oleksandr Chmyr <oleksandr.chmyr@nav.no>
2026-04-21 16:40:29 +0200

    Gjort SchedulerStatus serializable

--- src/main/kotlin/no/nav/helsemelding/messagegenerator/model/SchedulerStatus.kt
+++ src/main/kotlin/no/nav/helsemelding/messagegenerator/model/SchedulerStatus.kt
@@ -2,7 +2,9 @@ package no.nav.helsemelding.messagegenerator.model
 
 import kotlinx.datetime.Instant
 import kotlin.time.Duration
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class SchedulerStatus(
     val enabled: Boolean,
     val interval: Duration,
